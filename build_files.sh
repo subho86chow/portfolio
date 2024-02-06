26fb4cceef86c425cb9a95967c871062967b7a0e
Shubhajit Chowdhury <44286007+subho86chow@users.noreply.github.com>
2024-02-06 15:19:57 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,2 @@
-echo " BUILD START"
-python3 -c "import _sqlite3"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
+python3.9 manage.py collectstatic
