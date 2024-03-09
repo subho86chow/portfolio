2972b88c99b4a432b0bd920ecd1d25d1dab3611a
Shubhajit Chowdhury <subho86chow@email.com>
2024-03-09 20:11:55 +0530

    Bugfix for vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
 python3.9 -m pip install -r requirements.txt
+
+cp -r media/* staticfiles_build/media
 python3.9 manage.py collectstatic --noinput
---
+++ build_media.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Copy media files to staticfiles_build/media
+cp -r media/* staticfiles_build/media
+
+# You can add additional build steps here if needed (e.g., collectstatic)
